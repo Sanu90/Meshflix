31d5e5ed8de6bfdb094a5ba29ec2c2f58801a4c6
Sanup Divakaran <sanukyr@gmail.com>
2024-09-07 10:40:17 +0530

    Designed Index page and sign in page using html,css and basic React

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./Login.css";
+import logo from "../../assets/logo.png";
+
+const Login = () => {
+  const [signState, setSignState] = useState("Sign In");
+
+  return (
+    <div className="login">
+      <img src={logo} className="login-logo" alt="Netflix" />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+          {signState === "Sign Up" ? (
+            <input type="text" placeholder="Enter your Name" />
+          ) : (
+            <></>
+          )}
+
+          <input type="email" placeholder="Enter your Email" />
+          <input type="password" placeholder="Enter your Password" />
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor=" ">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          {signState === "Sign In" ? (
+            <p>
+              New to Netflix? <span>Sign Up</span>{" "}
+            </p>
+          ) : (
+            <p>
+              Already have Account? <span>Sign In</span>{" "}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
